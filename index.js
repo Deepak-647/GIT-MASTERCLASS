1ba3294eeb74d1ed8ad287e4e9d58857cf4df1f3
Deepak <dbehera647@gmail.com>
2023-08-29 09:57:52 +0530

    fetching some details about product deom json file to html

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const http = require('http');
 const fs =require('fs')
 
 const index =fs.readFileSync('index.html','utf-8');
-const data = fs.readFileSync('data.json','utf-8');
- 
+const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
+const product =data.products[0];
+
 const server =http.createServer((req,res)=>{
 //request url
 console.log(req.url)
 
-    console.log("Server started")
-    res.setHeader('Dummmy','dummyvalue')
-    // res.end('heloo');
-res.setHeader('Content-Type',"application/json")
-    res.end(data);
+switch(req.url){
+    case '/' :
+        res.setHeader('Content-Type',"text/html")
+        res.end(index)
+        break;
+    case '/api' :
+        res.setHeader('Content-Type',"application/json")
+        res.end(JSON.stringify(data))
+        break;
+    case '/product' :
+        res.setHeader('Content-Type',"text/html")
+        let modifiedIndex =index.replace("**title**",product.title).replace("**price**",product.price).replace("**url**",product.thumbnail).replace("**rating**",product.rating)
+        res.end(modifiedIndex)
+        break;
+    default:
+    res.writeHead(404,'Not Found')
+    res.end()
+
+}
+
+
+
+   
 })
 
 server.listen(8080);
