4e1cf3419d03a0066671dea71f26e478c5c98720
Deepak <dbehera647@gmail.com>
2023-08-29 09:00:10 +0530

    sending a static file from server

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const fs =require('fs')
 
+const index =fs.readFileSync('index.html','utf-8')
 const data = {age:5}
 const server =http.createServer((req,res)=>{
 //request url
 console.log(req.url)
+
     console.log("Server started")
     res.setHeader('Dummmy','dummyvalue')
     // res.end('heloo');
-    res.setHeader('Content-Type','application/json')
-res.end(JSON.stringify(data));
+
+    res.end(index);
 })
 
 server.listen(8080);
