32c6df34622bbc3132e1db472799863f16821e90
Deepak <dbehera647@gmail.com>
2023-08-29 11:39:42 +0530

    created the server rendered site

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const fs =require('fs')
 
 const index =fs.readFileSync('index.html','utf-8');
 const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
-const product =data.products[0];
+const products =data.products;
 
 const server =http.createServer((req,res)=>{
 //request url
-console.log(req.url)
+
+
+if(req.url.startsWith('/product')){
+    const id=req.url.split('/')[2]
+    const product=  products.find(p=>p.id === (+id))
+    res.setHeader('Content-Type',"text/html")
+    let modifiedIndex =index
+    .replace("**title**",product.title)
+    .replace("**price**",product.price)
+    .replace("**url**",product.thumbnail)
+    .replace("**rating**",product.rating)
+    res.end(modifiedIndex);
+    return;
+   
+}
 
 switch(req.url){
     case '/' :
