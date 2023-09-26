a217cbb231ef2924cc264d90802060649691bf86
Deepak <dbehera647@gmail.com>
2023-09-26 12:49:42 +0530

    created a router for products

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-const productController = require('./controller/product')
+const productRouter = require('./routes/products')
 
 //bodyParser
 server.use(express.json());
 server.use(morgan('default'))
 server.use(express.static('public'));
-
+server.use('/products',productRouter.router)
 
 
 // API - Endpoint - Route
@@ -15,17 +15,7 @@ server.use(express.static('public'));
 // Products
 // API ROOT , base URL, example - google.com/api/v2/
 
-//Create POST /products     C R U D
-server.post('/products', productController.createProduct);
-server.get('/products',productController.getProducts );
-// Read GET /products/:id
-server.get('/products/:id', productController.getProduct);
-// Update PUT /products/:id
-server.put('/products/:id', productController.replaceProduct);
-// Update PATCH /products/:id
-server.patch('/products/:id', productController.updateProduct);
-// Delete DELETE /products/:id
-server.delete('/products/:id', productController.deleteProduct);
+
 
 
 server.listen(8080, () => {
