aeed2d683e134242fd1624712d4046495ec8fe0a
Deepak <dbehera647@gmail.com>
2023-10-04 11:46:57 +0530

    validating Schema

--- model/product.js
+++ model/product.js
@@ -3,14 +3,14 @@ const { Schema } = mongoose;
 
 //Schema
 const productSchema = new Schema({
-        title: String,
+        title: {type:String,required:true},
         description: String,
-        price: Number,
-        discountPercentage: Number,
-        rating: Number,
-        brand: String, 
-        category: String,
-        thumbnail: String,
+        price: {type:Number , min:[0,'wrong price'],required:true},
+        discountPercentage: {type:Number , min:[0,'wrong price'],max:[5,'wrong max rating'],required:true},
+        rating: {type:Number , min:[0,'wrong price'],max:[5,'wrong max rating'],required:true},
+        brand: {type:String ,required:true}, 
+        category: {type:String ,required:true}, 
+        thumbnail: {type:String ,required:true}, 
         images: [ String ]
   });
   
